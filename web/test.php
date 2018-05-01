8adb4921c519103affd58c2753076d1859536781
skyler <skyler.blumenthal@gmail.com>
2018-05-01 16:46:11 -0600

    more test

--- web/test.php
+++ web/test.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-
 </head>
 <body>
 	<p>This is a test</p>
 	<?php
 	for ($x = 0; $x <= 10; $x++)
-		echo "<div class=\"div$x\">Div $x</div>";
+	{
+		if ($x % 2 == 0)
+			echo "<div class=\"div$x\">Div $x</div>";
+		else
+			echo "<div class=\"div$x\" style=\"color:red\">Div $x</div>";
+	}
 
 	?>
 </body>
