ece4cc8f52e4ad1524414fe8aae8da9887e8c87c
skyler <skyler.blumenthal@gmail.com>
2018-05-14 01:36:11 -0600

    ugh

--- web/add.php
+++ web/add.php
@@ -13,6 +13,7 @@ array_push($_SESSION["itemPrice"], $iPrice);
 // $_SESSION["itemName"] = $iName;
 // $_SESSION["itemPrice"] = $iName;
 echo "Adding item... ";
+print_r($_SESSION["itemName"]);
 
 echo '<meta http-equiv="refresh" content="1; URL=session.php" />';
 
