5eef4436fe1a660fc9065b383dc9ba334f5115ef
skyler <skyler.blumenthal@gmail.com>
2018-05-31 15:34:58 -0600

    php database lesson

---
+++ web/course.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Courses overview</title>
+</head>
+<body>
+	<h1> Courses </h1>
+	<ul>
+		<li><p>Course 1</p></li>
+		<li><p>Course 2</p></li>
+		<li><p>Course 3</p></li>
+		<li><p>Course 4</p></li>
+	</ul>
+</body>
+
+</html>
