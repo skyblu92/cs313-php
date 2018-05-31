b998aabfef7e82408b7ee87078f97a5339d09ae6
skyler <skyler.blumenthal@gmail.com>
2018-05-31 17:29:19 -0600

    bojkenoven

--- web/courseDetails.php
+++ web/courseDetails.php
@@ -5,7 +5,7 @@ $courseId = htmlspecialchars($_GET["course_id"]);
 
 $db = get_db();
 
-$query = "SELECT name, number FROM course WHERE id =$:id";
+$query = "SELECT name, number FROM course WHERE id =:id";
 
 $statement = $db->prepare($query);
 $statement->bindValue(":id", $courseId);
