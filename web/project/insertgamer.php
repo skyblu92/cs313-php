3792ec78bd158324c2f0ba61b7b8ce68ecfbebcc
skyler <skyler.blumenthal@gmail.com>
2018-06-05 15:54:26 -0600

    testing insertion

--- web/project/insertgamer.php
+++ web/project/insertgamer.php
@@ -32,7 +32,7 @@ $statement2->bindValue(":uId", $newId, PDO::PARAM_INT);
 $statement2->bindValue(":gtag", $gtag, PDO::PARAM_STR);
 
 $statement2->execute();
-header("Location: https://peaceful-hamlet-32303.herokuapp.com/project/gamesList.php?game_id=" . $gameId);
+header("Location: https://peaceful-hamlet-32303.herokuapp.com/project/gamesList.php?game_id=);$gameId");
 
 
 ?>
