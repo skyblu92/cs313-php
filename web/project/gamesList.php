fa1b4f86baf290305a9173b4337c9d3f5f271fd3
skyler <skyler.blumenthal@gmail.com>
2018-06-11 11:20:01 -0600

    putting in uiKit

--- web/project/gamesList.php
+++ web/project/gamesList.php
@@ -33,6 +33,10 @@ $users = $statement3->fetchAll(PDO::FETCH_ASSOC);
 <html>
 <head>
 	<title>Games overview</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/uikit.min.css" />
+        <script src="js/uikit.min.js"></script>
+        <script src="js/uikit-icons.min.js"></script>
 </head>
 <body>
 		<?php
