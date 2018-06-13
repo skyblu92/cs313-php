259a5f677a06bcbb7fa9a6a3b6bd9ce56dcaf037
skyler <skyler.blumenthal@gmail.com>
2018-06-13 00:17:31 -0600

    final push

--- web/gamers.sql
+++ web/gamers.sql
@@ -24,22 +24,27 @@ CREATE TABLE users_games
 	id SERIAL PRIMARY KEY,
 	userID INT REFERENCES users(id) NOT NULL,
 	gameID INT REFERENCES games(id) NOT NULL,
-	gamertag VARCHAR(50) NOT NULL
+	gamertag VARCHAR(50)
 
 );
 
 INSERT INTO games(name, system_name) VALUES ('League of Legends', 'PC');
-INSERT INTO games(name, system_name) VALUES ('Fortnite', 'PC');
 INSERT INTO games(name, system_name) VALUES ('StarCraft 2', 'PC');
-INSERT INTO games(name, system_name) VALUES ('Call of Duty: Infinite Warfare', 'PS4');
+INSERT INTO games(name, system_name) VALUES ('Fortnite', 'PC');
 
-INSERT INTO users(firstName, lastName, gamertag, notes) VALUES ('Skyler', 'Blumenthal'
-																, 'jimbob6'
-																, 'I am ranked Silver 2 in League of Legends. I also love to play Super Smash Bros!');
-INSERT INTO users(firstName, lastName, gamertag, notes) VALUES ('Jimbo', 'McGee'
-																, 'userName555'
-																, 'Love me some Tryndamere.');
-INSERT INTO users(firstName, gamertag, notes) VALUES ('Johnathon'
-														, 'hiddenUser355'
-														, 'you dont need to know my last name. Also, come at me in PUBG fam');
+INSERT INTO games(name, system_name) VALUES ('Fortnite', 'PS4');
+INSERT INTO games(name, system_name) VALUES ('Fortnite', 'Xbox One');
+INSERT INTO games(name, system_name) VALUES ('Call of Duty: Infinite Warfare', 'PS4');
+INSERT INTO games(name, system_name) VALUES ('Call of Duty: Infinite Warfare', 'Xbox One');
+
+INSERT INTO games(name, system_name) VALUES ('Player Unknown\'s Battlegrounds', 'PC');
+INSERT INTO games(name, system_name) VALUES ('Player Unknown\'s Battlegrounds', 'Xbox One');
+
+INSERT INTO games(name, system_name) VALUES ('Super Smash Bros', 'Wii U');
+INSERT INTO games(name, system_name) VALUES ('Settlers of Catan', 'Tabletop');
+
+
+INSERT INTO users(firstName, lastName, notes) VALUES ('Skyler', 'Blumenthal', 'I am ranked Silver 2 in League of Legends.');
+INSERT INTO users(firstName, lastName, notes) VALUES ('Jimmy', 'McGee', 'Love me some CS 313.');
+INSERT INTO users(firstName, notes) VALUES ('Johnathon', 'you dont need to know my last name. Also, come at me in PUBG fam.');
 
